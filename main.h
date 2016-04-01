91ecdc5995a6cb50dc1a4d65ea90c880a49e63ef
Robin Solignac <robin.solignac@epfl.ch>
2016-03-31 21:43:39 -0400

    add main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  Project
+//
+//  Created by Robin Solignac on 21/03/2016.
+//  Copyright © 2016 Robin Solignac. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+
+#endif /* main_h */
