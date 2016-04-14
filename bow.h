cbef37ef406bd548661524f2af2c6c444216faca
Sidney Ng <yendisng@gmail.com>
2016-04-14 02:42:38 -0400

    bag of words regular recognition rate calc fixed; adding in bag of words probabilistic

--- bow.h
+++ bow.h
@@ -6,7 +6,9 @@
 #include <opencv2/nonfree/nonfree.hpp>
 #include "QMULset.h"
 
-void kFoldsCrossValidation(QMULset Dataset, const int numCodewords);
+const double pi = 3.1415926;
+
+void BoWkFoldsCrossValidation(QMULset Dataset, const int numCodewords, bool prob);
 void TrainBoW(vector< vector<Mat> > people_set,
               Mat &codeBook,
               vector<vector<Mat>> &imageDescriptors,
@@ -19,5 +21,20 @@ void TestBoW(vector< vector<Mat> > people_set,
              const int partitionStart,
              const int partitionSize,
              double &recognition);
+void TrainBoWProb(vector< vector<Mat> > people_set,
+                  Mat &codeBook,
+                  vector<vector<Mat>> &imageDescriptors,
+                  const int numCodewords,
+                  const int partitionStart,
+                  const int partitionSize,
+                  vector<Mat> &covar_mat,
+                  vector<Mat> &mean_vectors);
+void TestBoWProb(vector< vector<Mat> > people_set,
+                 const Mat codeBook,
+                 const int partitionStart,
+                 const int partitionSize,
+                 const vector<Mat> covar,
+                 const vector<Mat> mean_vector,
+                 double &recognition);
 
 #endif // BOW_H
